516487fe3485f428b055e14372538a4facba68a7
Joshua Thompson <ysovuka@gmail.com>
2018-01-17 20:52:05 -0600

    Created new web project for the core website of the knowledge base.

---
+++ src/Pundit.KnowledgeBase.WebCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pundit.KnowledgeBase.WebCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
